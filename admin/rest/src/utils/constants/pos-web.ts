65a24a2e4373fd41d8d756d73f8ad70b3721050c
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-03-20 23:07:23 +0530

    pos_web constants

---
+++ admin/rest/src/utils/constants/pos-web.ts
@@ -0,0 +1,106 @@
+
+const unitOptions = [
+    {label: "inch", value: "inch"},
+    {label: "mm", value: "mm"},
+    {label: "cm", value: "cm"},
+    {label: "gm", value: "gm"},
+    {label: "kg", value: "kg"},
+    {label: "NOS", value: "nos"}
+];
+
+const existingProduct = {
+    barcodeNo: Date.now(),
+    productName: "Dummy Product 001",
+    supplierName: "Bhagwan Singh",
+    designNumber: 56020001,
+    sizeWeight: 10,
+    sizeWeightUnit: {label: "gm", value: "gm"},
+    availableQuantity: 5,
+    purchasePrice: 1500,
+    salePrice: 500
+}
+
+const newProduct = {
+    barcodeNo: Date.now(),
+    productName: "New product",
+    supplierName: "New supplier",
+    designNumber: 0,
+    sizeWeight: 0,
+    sizeWeightUnit: {label: "mm", value: "mm"},
+    availableQuantity: 1,
+    purchasePrice: 0,
+    salePrice: 0
+}
+
+const existingWebProduct = {
+    barcodeNo: "999990",
+    productGalleryImage: "demo/demo.jpg",
+    productMediaItems: ['demo/demo.jpg'],
+    productName: "New web product",
+    productDescription: "New dummy product",
+    productSizeWeight: { value: 10, unit: "gm" },
+    supplierName: "Sunny Shewatabh",
+    designNumber: 12345,
+    width: 5,
+    height: 5,
+    brandMenufacturer: "Ecorpin Corp.",
+    availableQuantity: {},
+    purchasePrice: 1099,
+    salePrice: 999,
+    tags: "string",
+    variationFg: false,
+    variationData: {},
+    category: "string",
+}
+
+const newWebProduct = {
+    barcodeNo: "999990",
+    productGalleryImage: "demo.jpg",
+    productMediaItems: [],
+    productName: "New Product",
+    productDescription: "A dummy product.",
+    productSizeWeight: 10,
+    productSizeWeightUnit: "inch",
+    supplierName: "needyHub Pvt. Ltd.",
+    designNumber: 105,
+    width: 12,
+    height: 12,
+    brandMenufacturer: "NeedyHub",
+    availableQuantity: 21,
+    productQuantityUnit: "nos",
+    purchasePrice: 999,
+    salePrice: 995,
+    tags: "",
+    variationFg: false,
+    variationData: {},
+    category: "Showpeace",
+}
+
+export {
+    unitOptions,
+    newProduct,
+    existingProduct,
+    newWebProduct,
+    existingWebProduct
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
