ce5f07d04022200ab124bec2a68aef71bd358949
BhagwanSingh3035 <bhagwan@joinnextmed.com>
2023-04-02 15:50:36 +0530

    api added for pos syatem

--- admin/rest/src/assets/pos_web/api/posApis.ts
+++ admin/rest/src/assets/pos_web/api/posApis.ts
@@ -1,18 +1,24 @@
+import axios from "axios";
 
-export const getPosProducts = () => {
+const posProductsApi = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
+})
 
+export const getPosProducts = async () => {
+    const response = await posProductsApi.get("/pos-products");
+    return response.data;
 }
 
-export const createPosProduct = () => {
-
+export const createPosProduct = async (formData: any) => {
+    return await posProductsApi.post("/pos-products", formData);
 }
 
-export const updatePosProduct = () => {
-
+export const updatePosProduct = async (formData: any) => {
+    return await posProductsApi.patch(`/pos-products/${formData?.id}`, formData);
 }
 
-export const deletePosProduct = () => {
-
+export const deletePosProduct = async ({ id }: { id: string }) => {
+    return await posProductsApi.delete(`/pos-products/${id}`, id);
 }
 
-// export default getPosProducts;
+export default posProductsApi;
---
+++ admin/rest/src/assets/pos_web/api/webApis.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const webProductsApi = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
+})
+
+export const getWebProducts = async () => {
+    const response = await webProductsApi.get("/web-products");
+    return response.data;
+}
+
+export const createWebProduct = async (formData: any) => {
+    return await webProductsApi.post("/web-products", formData);
+}
+
+export const updateWebProduct = async (formData: any) => {
+    return await webProductsApi.patch(`/web-products/${formData?.id}`, formData);
+}
+
+export const deleteWebProduct = async ({ id }: { id: string }) => {
+    return await webProductsApi.delete(`/web-products/${id}`, id);
+}
+
+export default webProductsApi;
